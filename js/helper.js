f11b5da0f4de4dcdcee7aea3eefaab4b940caa89
Robert Pastorelli <pastorer@bc.edu>
2019-12-11 11:03:42 -0500

    editing vega-lite chart size

--- js/helper.js
+++ js/helper.js
@@ -4,31 +4,16 @@ let restSpec = {
     $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
     data: {"url" : "data/NumRest.csv"},
     background: "#D3D3D3",
-    hconcat: [
-        {
             title: "Number of Fast Food Restaurants",
-            mark: {type: 'line', point: true, tooltip: true},
-            width: 500,
+            mark: {type: 'line', point: {filled: false, fill: "white"}, tooltip: true},
+            width: 600,
+            height: 600,
             encoding: {
                 x: {field: "Year", type: "temporal", axis: {title:  "Year"}, sort: "y"},
                 y: {field: "NumberRest", type: "quantitative", 
-                        axis: {title: "Number of Restaurants"}, scale: {domain: [220000, 250000]}},
+                        axis: {title: "Number of Restaurants"}, format: ",", scale: {domain: [220000, 250000]}},
                 color: {value: "#BD8BC3"}
             }
-        },
-        {
-            title: "Number of McDonald's In the US",
-            mark: {type: 'line', point: true, tooltip: true},
-            width: 500,
-            encoding: {
-                x: {field: "Year", type: "temporal", axis: {title:  "Year"}, sort: "y"},
-                y: {field: "NumMC", type: "quantitative", 
-                        axis: {title: "Number of McDonalds",}, scale: {domain: [32000, 39000]}},
-                color: {value: "#BD8BC3"}
-            }
-            
-        }
-    ]
 };
 vegaEmbed('#vega-area', restSpec);
 
